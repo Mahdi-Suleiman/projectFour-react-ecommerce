9c627002be6171a94dbcde435f5bc5822cdb78dd
GhassanDabak <ghassan.h.dabak@gmail.comgit config --global user.name GhassanDabakgit config --global user.email ghassan.h.dabak@gmail.com>
2021-12-05 21:48:20 +0200

    fixed bugs

--- projectfour/src/components/card/card.containter.jsx
+++ projectfour/src/components/card/card.containter.jsx
@@ -1,23 +1,35 @@
 import React from 'react'
 import AddToCart from '../cart/addtocart'
+import {  Link } from 'react-router-dom';
 import DeleteCard from './delete.card'
-export default function CardContainer(props) {
-    return (
+export default class CardContainer extends React.Component {
+    constructor(props){
+        super(props)
+        
+    }
 
-        // PRODUCTS PAGE 
-        <div className="card">
-            <img src={props.value.img} />
-            <h3>{props.value.title}</h3>
-            <p>{props.value.shortDesc}</p>
-            <h6>{props.value.price}</h6>
-            
-            {
-                props.role === "user" ?
-                    // <DeleteCard buttonValue="Add to cart" index={props.index} plusCounter={props.plusCounter} imgURL={props.value.img} role="user" /> 
-                    <AddToCart index={props.index} plusCounter={props.plusCounter} />:
-                    // <DeleteCard buttonValue="Delete Item" index={props.index} imgURL={props.value.img} role="admin" />
-                    <button onClick={props.deleteCard}> Delete Card</button>
-            }
-        </div>
-    )
+    render(){
+        
+            return (
+                
+                // PRODUCTS PAGE 
+                <div className="card">
+                <img src={this.props.value.img} />
+                <h3>{this.props.value.title}</h3>
+                <p>{this.props.value.shortDesc}</p>
+                <h6>{this.props.value.price}</h6>
+                {
+
+                    
+                    this.props.role === "user" ?
+                    // <DeleteCard buttonValue="Add to cart" index={this.props.index} plusCounter={this.props.plusCounter} imgURL={this.props.value.img} role="user" /> 
+                    <AddToCart handleAddToCart={this.props.handleAddToCart} index={this.props.index} plusCounter={this.props.plusCounter} />:
+                        // <DeleteCard buttonValue="Delete Item" index={this.props.index} imgURL={this.props.value.img} role="admin" />
+                        <button onClick={this.props.deleteCard}> Delete Card</button>
+                
+                }
+            </div>
+            )
+    }
+    
 }
