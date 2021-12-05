c0856950bf5f82227dc344aa3bc2984147e81fc5
Mahdi-Suleiman <mahdi.suleimann@gmail.com>
2021-12-05 05:32:11 +0200

    .

--- projectfour/src/App.js
+++ projectfour/src/App.js
@@ -5,10 +5,30 @@ import UserProfile from './components/userprofile/userprofile';
 import React from 'react';
 import Admin from './components/admin/admin';
 import Card from './components/card/card';
+import AddToCart from './components/cart/addtocart';
+import Cart from './components/cart/cart';
+import CheckoutButton from './components/cart/checkout.button';
 
 class App extends React.Component {
     constructor() {
         super();
+
+        this.state = {
+            counter: 0,
+            totalPrice: JSON.parse(localStorage.getItem('total'))
+        }
+    }
+
+    plusCounter = (price) => {
+        if (JSON.parse(localStorage.getItem('loggedUser'))) {
+            this.setState({ counter: this.state.counter + 1 })
+            this.setState({totalPrice:JSON.parse(localStorage.getItem('total'))})
+            this.setState({totalPrice: Number(this.state.totalPrice) + Number(price)})
+            console.log("hello" + typeof(this.state.totalPrice))
+            localStorage.setItem('total', JSON.stringify(this.state.totalPrice))
+            console.log(typeof(this.state.totalPrice))
+            console.log("hello" + typeof(price))
+        }
     }
 
     // editUserData = (e,firstname,lastname,email,password) => {
@@ -35,11 +55,14 @@ class App extends React.Component {
     render() {
         return (
             <div>
-                {/* <Login /> */}
-                {/* <Registration /> */}
-                {/* <UserProfile /> */}
-                {/* <Card /> */}
-                {/* <Admin /> */}
+                <Login />
+                <Registration />
+                <UserProfile />  
+                 {/* <Card plusCounter={this.plusCounter} /> */}
+                {/* <Admin counter={this.state.counter} totalPrice={this.state.totalPrice}/>  */}
+                {/* <Cart />  */}
+                <CheckoutButton totalPrice={this.state.totalPrice} counter={this.state.counter}/>
+
             </div>
         );
     }
